c26c787a9c4920cd06c2768ab16b9ff2f4967387
pramod kumar <pamu098@gmail.com>
2021-06-11 15:37:46 +0530

    new updates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,22 +6,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
 
 
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-        return new Response('we have a very good controller');
+        /*
+        // fun example to use the service directly
+        $html= $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html); */
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
 
 
@@ -33,6 +38,8 @@ class QuestionController extends AbstractController
             'Then download the template using bootstrap',
             'Then think of th contents needs to be added'    
         ];
+
+        dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
             'question'=> ucwords(str_replace('-',' ',$slug)),
