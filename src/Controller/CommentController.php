675502e2ef1bf0f1d724a24ce139d674606c0f57
pramod kumar <pamu098@gmail.com>
2021-06-11 15:41:09 +0530

    new updates

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+
+    /**
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
+     */
+
+    public function commentVote($id, $direction, LoggerInterface $logger)
+    {
+
+        if($direction === 'up') {
+            $logger->info('voting up!');
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $logger->info('voting down!');
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return new JsonResponse(['votes' => $currentVoteCount]);
+
+    }
+
+
+
+}
+
